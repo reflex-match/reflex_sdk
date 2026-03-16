9bf7e1f56b2bace109bc468be2a7a06a101d55d2
Swann Williame <swann.williame@gmail.com>
2026-03-16 10:15:10 +0100

    base api python oris

--- oris_python/main.py
+++ oris_python/main.py
@@ -1,5 +1,5 @@
 import logging
-from oris import Oris
+from oris import Oris, ensure_database_exists
 from dotenv import load_dotenv
 import os
 from sqlalchemy import create_engine
@@ -10,27 +10,52 @@ warnings.simplefilter("ignore")
 logger = logging.getLogger(__name__)
 
 def main():
-    logging.basicConfig(filename='', level=logging.INFO, format="%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+    logging.basicConfig(
+        filename='',
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+    )
+
     load_dotenv()
-    engine = create_engine(f'postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB')}')
-    
-    # Check connection
+
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    db_host = os.getenv("DB_HOST")
+    db_name = os.getenv("DB")
+    db_port = os.getenv("DB_PORT", 5432)
+
+    ensure_database_exists(
+        dbname=db_name,
+        user=db_user,
+        password=db_password,
+        host=db_host,
+        port=int(db_port)
+    )
+
+    engine = create_engine(
+        f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+    )
+
     try:
         connection = engine.connect()
         logger.info("Connection to PostgreSQL established successfully!")
     except Exception as e:
         logger.error(f"Connection failed! Error: {e}")
+        return
 
     bdds = [
-        ("Agency_Document", "doc_reflex/1_data/sdk/agency", "yes")
+        ("Agency_Document", "doc_reflex/1_data/bd/agency", "no")
     ]
 
-    client = Oris(os.getenv('ORIS_URL'))
-    client.connect(os.getenv('ORIS_USER'), os.getenv('ORIS_PASSWORD'))
+    client = Oris(os.getenv("ORIS_URL"))
+    client.connect(os.getenv("ORIS_USER"), os.getenv("ORIS_PASSWORD"))
 
     for bdd in bdds:
         df = client.get_db_as_dataframe(bdd[0], bdd[1], bdd[2])
-        df.to_sql(f'os_{bdd[0].lower()}', connection, if_exists='replace', index=False)
+        df.to_sql(f"os_{bdd[0].lower()}", connection, if_exists="replace", index=False)
 
-if __name__ == '__main__':
+    connection.close()
+    logger.info("Process completed successfully.")
+
+if __name__ == "__main__":
     main()
--- oris_python/oris.py
+++ oris_python/oris.py
@@ -2,12 +2,39 @@ import logging
 import requests
 import xml.etree.ElementTree as ET
 import pandas as pd
+import psycopg2
+from psycopg2 import sql
 
 logger = logging.getLogger(__name__)
 
+def ensure_database_exists(
+    dbname: str,
+    user: str,
+    password: str,
+    host: str = "localhost",
+    port: int = 5432,
+):
+    conn = psycopg2.connect(
+        dbname="postgres",
+        user=user,
+        password=password,
+        host=host,
+        port=port,
+    )
+    conn.autocommit = True
+    try:
+        with conn.cursor() as cur:
+            cur.execute("SELECT 1 FROM pg_database WHERE datname = %s;", (dbname,))
+            if cur.fetchone() is None:
+                cur.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(dbname)))
+                logger.info(f'Database "{dbname}" created')
+            else:
+                logger.debug(f'Database "{dbname}" already exists')
+    finally:
+        conn.close()
+
 def safe_to_numeric(ser: pd.Series):
     try:
-        
         tmp = ser.str.replace("-", "0")
         tmp = tmp.str.replace(",", ".", regex=True)
         tmp = pd.to_numeric(tmp, errors="coerce")
@@ -33,41 +60,28 @@ def infer_formule_type(ser: pd.Series):
     except:
         logger.debug(f'Formule "{ser.name}" is string')
         return ser
-    
+
 class Oris:
     """Python client for Oris
     """
     def __init__(self, url="https://reflex.link", verify_ssl=True):
-        """Init Oris client
-
-        Args:
-            url (str, optional): URL to Oris. Defaults to "https://www.x-oris.com".
-            verify_ssl (bool, optional): verify if connection is secure. Defaults to True.
-        """
         self._url = url
         self._verify_ssl = verify_ssl
         self._id = None
 
     def connect(self, user: str, passwd: str):
-        """Connect to oris backend
-
-        Args:
-            user (str): username
-            passwd (str): password
-
-        Returns:
-            str: token id
-        """
-
-        response = requests.get(f"{self._url}/form0001?user={user}&pass={passwd}&xml=true", verify=self._verify_ssl)
+        response = requests.get(
+            f"{self._url}/form0001?user={user}&pass={passwd}&xml=true",
+            verify=self._verify_ssl
+        )
         root = ET.fromstring(response.text)
         self._id = root.get("id")
-        if(self._id == None):
+        if self._id is None:
             logger.warning(f'Unable to connect {user} to Oris')
         else:
             logger.info(f'{user} connected to Oris')
 
-    def get_db(self, db: str, db_path: str, archives = "no"):
+    def get_db(self, db: str, db_path: str, archives="no"):
         headers = {
             "User-Agent": "Python",
             "Accept": "application/json",
@@ -77,52 +91,51 @@ class Oris:
         }
 
         response = requests.get(f"{self._url}/rest/{db}s?glob={archives}", headers=headers)
-        if(response.status_code == 200):
+        if response.status_code == 200:
             logger.info(f'{db} received')
         else:
             logger.error(f'Unable to get {db} at {db_path}')
 
         return response.json().get(f'{db.lower()}s')
-    
+
     def get_db_params(self, db: str, db_path: str):
         headers = {
             "User-Agent": "Python",
             "Accept": "application/json",
             "Content-Type": "application/json; charset=utf-8",
             "X-Oris-Basepath": f"{self._url}/{self._id}/{db_path}",
-            "Referer": f"{self._url}/{self._id}/"
+            "Referer": f"{self._url}/{self._id}"
         }
 
         response = requests.get(f"{self._url}/rest/{db}?readparam=true", headers=headers)
-        if(response.status_code == 200):
+        if response.status_code == 200:
             logger.info(f'{db} parameters received')
         else:
             logger.error(f'Unable to get {db} parameters at {db_path}')
 
-        # logger.debug(params)
         return response.json().get("champs")
-    
-    def get_db_as_dataframe(self, db: str, db_path: str, archives = "no"):
+
+    def get_db_as_dataframe(self, db: str, db_path: str, archives="no"):
         params = self.get_db_params(db, db_path)
-        
+
         col_idx = {}
         col_date_idx = []
         col_number_idx = []
         col_bool_idx = []
         col_formule_idx = []
-        
+
         for champ in params:
-            col_name = champ.get('name').lower().replace(" ", "_").replace("'","") + '_' +champ.get('id')
+            col_name = champ.get('name').lower().replace(" ", "_").replace("'", "") + '_' + champ.get('id')
             col_idx[champ.get('idrest')] = col_name
-            if(champ.get("type") == "date"):
+            if champ.get("type") == "date":
                 col_date_idx.append(col_name)
-            if(champ.get("type") == "bool"):
+            if champ.get("type") == "bool":
                 col_bool_idx.append(col_name)
-            if(champ.get("type") == "bcd" or champ.get("type") == "heure"):
+            if champ.get("type") == "bcd" or champ.get("type") == "heure":
                 col_number_idx.append(col_name)
-            if(champ.get("type") == "formule"):
+            if champ.get("type") == "formule":
                 col_formule_idx.append(col_name)
-        
+
         logger.debug(f'column indexes: {col_idx}')
         logger.debug(f'date columns: {col_date_idx}')
         logger.debug(f'number columns: {col_number_idx}')
